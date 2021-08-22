7b8f14f9d9ecb8cdbe6a489654caa042a3519293
CHAUHAN <pratap.chauhan.ext@siemens.com>
2021-08-22 21:40:04 +0530

    api url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverUrl: "http:// ec2-3-143-219-144.us-east-2.compute.amazonaws.com"
+  serverUrl: "http://ec2-3-143-219-144.us-east-2.compute.amazonaws.com/api"
 };
